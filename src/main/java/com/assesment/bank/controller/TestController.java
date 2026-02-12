8d34ac8f98dae96e6795c1a19ab79a1f36ea047d
iqrabbani <159766664+iqrabbani@users.noreply.github.com>
2026-02-12 13:43:31 +0700

    fix: update test controller

---
+++ src/main/java/com/assesment/bank/controller/TestController.java
@@ -0,0 +1,28 @@
+public
+package com.trollmarket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Statement;
+
+@RestController
+public class TestController {
+
+    private final DataSource dataSource;
+
+    public TestController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping("/sqli")
+    public String test(@RequestParam String id) throws Exception {
+        Statement stmt = dataSource.getConnection().createStatement();
+        stmt.executeQuery("SELECT * FROM users WHERE id=" + id);
+        return "OK";
+    }
+}TestController{
+
+}
